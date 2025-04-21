93897c5f3b825d3312535efe02b891d0822a3c20
Dwaysetya <154573487+Dwaysetya@users.noreply.github.com>
2025-04-21 11:15:08 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,9 @@
+// Join
+
+let nameSiswa1 = [
+  ["Dway", "Yusuf", "Reza", "Nanda"],
+  ["Osea", "Yasir", "Faisal"],
+];
+
+console.log(...nameSiswa1[0], ...nameSiswa1[1]); // manual
+console.log(nameSiswa1.join());
