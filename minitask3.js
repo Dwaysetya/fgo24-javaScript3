596b2b0cf9cf91786e5bdfd561843e25a9547c5f
Dwaysetya <154573487+Dwaysetya@users.noreply.github.com>
2025-04-21 14:25:35 +0700

    Add files via upload

--- minitask3.js
+++ minitask3.js
@@ -1,6 +1,6 @@
 const animal = "ayam,bebek,sapi,unta";
 
-let newAnimal = animal.split(",");
-newAnimal.splice(2, 1, "domba");
+// let newAnimal = animal.split(",").t;
+// newAnimal.splice(2, 1, "domba");
 // newAnimal[2] = "domba";
-console.log(newAnimal);
+console.log(animal.split(",").toSpliced(1, 2, "bebek", "domba"));
