5a5ec27a3b9417366b6c214d054dba672b0ed6e9
Dwaysetya <154573487+Dwaysetya@users.noreply.github.com>
2025-04-22 11:06:24 +0700

    Add files via upload

--- minitask5.js
+++ minitask5.js
@@ -10,6 +10,8 @@ function queue(name, time) {
   });
 }
 
+// Then - Chatch
+
 // queue("Jhon", 1500)
 //   .then(function (Callback) {
 //     console.log(Callback);
@@ -33,26 +35,14 @@ function queue(name, time) {
 //   });
 
 async function main() {
-  await queue("Jhon", 1500)
-    .then(function (Callback) {
+  await queue("Jhon", 1500).then(function (Callback) {
+    console.log(Callback);
+    queue("Ed", 2000).then(function (Callback) {
       console.log(Callback);
-      queue("Ed", 2000)
-        .then(function (Callback) {
-          console.log(Callback);
-          queue("Jane", 500)
-            .then(function (Callback) {
-              console.log(Callback);
-            })
-            .catch(function () {
-              console.log(Callback);
-            });
-        })
-        .catch(function () {
-          console.log(Callback);
-        });
-    })
-    .catch(function () {
-      console.log(Callback);
+      queue("Jane", 500).then(function (Callback) {
+        console.log(Callback);
+      });
     });
+  });
 }
 main();
